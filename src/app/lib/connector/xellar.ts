d82ed5a7a1bb2f512bbbadaf4905a7a7c69af5ee
Daviskelvin824 <daviskelvin824@gmail.com>
2025-07-23 10:53:35 +0700

    fix: xellar api

--- src/app/lib/connector/xellar.ts
+++ src/app/lib/connector/xellar.ts
@@ -5,7 +5,7 @@ import { Config } from "wagmi";
 export const config = defaultConfig({
   appName: "Xellar",
   walletConnectProjectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? "",
-  xellarAppId: process.env.XELLAR_API_KEY,
+  xellarAppId: process.env.NEXT_PUBLIC_XELLAR_API_KEY,
   xellarEnv: "sandbox",
   chains: [liskSepolia],
   ssr: true,
